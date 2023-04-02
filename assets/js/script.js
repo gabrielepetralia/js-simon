060a40a8f9fcba5ae276dc4d312378787627ce76
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-03 01:07:29 +0200

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 let randNumbers = [];
+let inputNumbers = [];
+let guessedNumbers = [];
 let min;
 let max;
+let points = 0;
 
 const genNum = document.getElementById("gen-num");
 const checkNum = document.getElementById("check-num")
-const numsRand = document.getElementsByClassName("num-rand");
-const numInput = document.querySelectorAll("input");
-const insNum = document.querySelector("h2");
+const numCellCollection = document.getElementsByClassName("num-rand");
+const numInputCollection = document.querySelectorAll("input");
+const heading = document.querySelector("h2");
+const output = document.querySelector(".message");
 
-insNum.classList.add("hide2");
 checkNum.classList.add("hide");
 
 genNum.addEventListener("click", () => {
-  toggleNumInput();
-  insNum.classList.add("hide2");
-  checkNum.classList.add("hide");
-  randNumbers = [];
+  reset();
   
   min = parseInt(document.getElementById("min-num").value);
   max = parseInt(document.getElementById("max-num").value);
@@ -33,6 +33,12 @@ genNum.addEventListener("click", () => {
   startSetTimeout()
 })
 
+checkNum.addEventListener("click", () => {
+  getNumInput();
+  check();
+  checkNum.classList.add("hide");
+})
+
 /*--------------- Functions ---------------*/
 function getRandomNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -44,7 +50,7 @@ function generateRandomNumbers () {
     num = getRandomNumber(min, max);
     if(!randNumbers.includes(num)) {
       randNumbers.push(num);
-      numsRand[i].innerHTML =  num;
+      numCellCollection[i].innerHTML =  num;
       i++;
     }
   }
@@ -52,17 +58,53 @@ function generateRandomNumbers () {
 
 function startSetTimeout() {
   setTimeout(function() {
-    for(let i=0 ; i<5 ; i++) {
-      numsRand[i].innerHTML = "";
+    for(let i=0 ; i<numCellCollection.length ; i++) {
+      numCellCollection[i].innerHTML = "";
     }
-    insNum.classList.remove("hide2");
+    heading.innerHTML = "Inserisci i numeri (tutti diversi)";
     checkNum.classList.remove("hide");
     toggleNumInput();
   }, 5000);
 }
 
 function toggleNumInput() {
-  for(let i=2 ; i<numInput.length ; i++) {
-    numInput[i].classList.toggle("hide");
+  for(let i=2 ; i<numInputCollection.length ; i++) {
+    numInputCollection[i].classList.toggle("hide");
+    numInputCollection[i].value="";
   }
+}
+
+function getNumInput() {
+  for(let i=2 ; i<numInputCollection.length ; i++) {
+    inputNumbers.push(parseInt(numInputCollection[i].value));
+  }
+}
+
+function check () {
+  for(let i=0 ; i<randNumbers.length ; i++) {
+    if(randNumbers.includes(inputNumbers[i])) {
+      points++;
+      guessedNumbers.push(inputNumbers[i]);
+    }
+
+    output.innerHTML = `Hai indovinato ${points} numeri :`;
+    for(let i=0; i<guessedNumbers.length ; i++) {
+      if(i===guessedNumbers.length - 1) {
+        output.innerHTML += ` ${guessedNumbers[i]}.`;
+      } else {
+        output.innerHTML += ` ${guessedNumbers[i]},`;
+      }
+    }
+  }
+}
+
+function reset(){
+  toggleNumInput();
+  heading.innerHTML = "Memorizza i numeri"
+  checkNum.classList.add("hide");
+  output.innerHTML="";
+  randNumbers = [];
+  inputNumbers= [];
+  guessedNumbers = [];
+  points = 0;
 }
